2193e6e7390c487c9410fcc698d9868faef5a65e
824178949@qq.com <ly950729>
2017-09-01 17:50:37 +0800

    修改短信接口

--- api/sms.php
+++ api/sms.php
@@ -38,14 +38,10 @@ if($member['code_time'] > $posttime) {
 } else {*/
     $code = random(6);//唯一吗，用于扩展验证
 /*}*/
-$msg = "验证码为$code,30分钟内有效。";
-$msg = urlencode($msg);
+$msg = urlencode("验证码为$code,30分钟内有效。");
 $url = "http://oa.jxglkf.com.cn:88/sms/sendsms.php?mobs=$mobile&msg=$msg";
-//print_r($url);
-//print_r(111);
-//$url = "http://oa.jxglkf.com.cn:88/sms/sendsms.php?mobs=18210582606&msg=hello";
-//print_r($url);
 $res=httpGet($url); //发送短信
+$res = explode(',',$res);
 print_r(json_encode($res));
 exit;
 $add_time	= time();
